234eb301663d0cecc006d4cf091da8d6b7edc8a9
Vishesh Kumar <mrvisheshk@gmail.com>
2026-02-17 20:08:22 +0530

    fixed socket bug

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client";
 export const createSocketConnection=()=>{
     const url=import.meta.env.VITE_BACKEND_URL;
-    return io(url,{withCredentials:true})
+    return io(url,{withCredentials:true,transports: ["websocket"]})
 }
